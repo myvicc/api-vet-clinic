8797bc2dbdbc85f24346b84591fc77e9acd5817f
myvicc <dir@acg.co.ua>
2023-06-07 17:29:51 +0300

    prettier

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
-import fastify from "fastify";
-import fastifySwagger from "@fastify/swagger";
-import fastifyMultipart from "@fastify/multipart";
-import fastifySwaggerUi from "@fastify/swagger-ui";
+import fastify from 'fastify';
+import fastifySwagger from '@fastify/swagger';
+import fastifyMultipart from '@fastify/multipart';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 
 export const application = fastify({
-    logger: true
-})
+  logger: true,
+});
 
 application.register(fastifyMultipart, {
-    addToBody: true
+  addToBody: true,
 });
 application.register(fastifySwagger);
 application.register(fastifySwaggerUi, {
-    routePrefix: '/docs'
-})
-
+  routePrefix: '/docs',
+});
--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export const connect = async () =>
-    mongoose.connect('mongodb://localhost:27019', {
-        user: 'mongo',
-        pass: 'mypass',
-    });
+  mongoose.connect('mongodb://localhost:27019', {
+    user: 'mongo',
+    pass: 'mypass',
+  });
